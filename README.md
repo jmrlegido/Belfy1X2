1fb583509731536e7de0935f903a0f2e89298e0b
juanma_vm <jmrlegido@gmail.com>
2015-08-28 14:01:15 +0200

    primera versión (5.1) con Android Studio

--- README.md
+++
@@ -1,4 +0,0 @@
-Belfy1x2
-========
-
-aplicación android de ayuda para hacer quinielas españolas
--- gen/jm/q1x2/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jm.q1x2;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
